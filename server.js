1f872aa76edea926ffd353ed3fcd6e0565458da4
Kh'ael <57403868+danielcosmin@users.noreply.github.com>
2020-09-08 16:53:12 +0200

    setting up body parser

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const app = express()
 const PORT = 6500
+const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
 
-//setup static folder for external files
+
+    //setup body-parser
+app.use(bodyParser.urlencoded({extended: true}))
+
+    //setup static folder for external files
 app.use(express.static( __dirname + '/public'))
-//View Engine Setups(TEMPLATE ENGINE SETUP)
+
+    //View Engine Setups(TEMPLATE ENGINE SETUP)
 app.set('view engine', 'hbs')
 
     //routes
 app.use('/', indexRouter)
-// app.get('/electronics', (res,req)=>{
-//     req.render('')
-// })
-//listening port
+
+
+    //listening port
 app.listen(PORT, ()=>{
     console.log('Server is running')
 })
