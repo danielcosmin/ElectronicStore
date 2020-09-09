cf06c4f07363d190127afe129f7ec898d59707d1
mohammad-joumah <mohammad.joumah@gmail.com>
2020-09-09 12:00:05 +0200

    server update

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = 6500
 const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser');
 
 //setup static folder for external files
-app.use(express.static( __dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
+
 //View Engine Setups(TEMPLATE ENGINE SETUP)
 app.set('view engine', 'hbs')
 
-    //routes
+// use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//routes
 app.use('/', indexRouter)
-// app.get('/electronics', (res,req)=>{
-//     req.render('')
-// })
-//listening port
-app.listen(PORT, ()=>{
+    // app.get('/electronics', (res,req)=>{
+    //     req.render('')
+    // })
+    //listening port
+app.listen(PORT, () => {
     console.log('Server is running')
 })
