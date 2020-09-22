459ec365849b9a02f7383326ce5b21dc3fc1403f
mohammad-joumah <mohammad.joumah@gmail.com>
2020-09-22 11:15:53 +0200

    some update with signup and sign in form and adding database

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const bodyParser = require('body-parser');
 const { request } = require('http');
 const { response } = require('express');
 const fetch = require('node-fetch');
+const mongoose = require('mongoose');
+const User = require('./models/userModel');
 
+mongoose.connect('mongodb+srv://admin:M0h@mmed@mohammad.v7aku.mongodb.net/test', { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        console.log('the mongodb is connected......')
+    })
+    .catch(err => console.log(err));
 
 
 let userJSON_data = fs.readFileSync('public/jsonFiles/userData.json', { root: 'public' });
@@ -33,12 +40,28 @@ app.listen(PORT, () => {
 //10.09.2020 -- Post data and saving to a JSON file
 
 app.post('/sign', (request, response) => {
-    let data_from_user = request.body
-        //console.log(userObject_data)
-    userObject_data.push(data_from_user)
-        //console.log(userObject_data)
-    fs.writeFileSync(__dirname + '/public/jsonFiles/userData.json', JSON.stringify(userObject_data))
-    response.render('index')
+    let userId = Math.floor(Math.random() * 100);
+    userInfo = request.body;
+    // let data_from_user = request.body
+    //     //console.log(userObject_data)
+    // userObject_data.push(data_from_user)
+    //     //console.log(userObject_data)
+    // fs.writeFileSync(__dirname + '/public/jsonFiles/userData.json', JSON.stringify(userObject_data))
+    // response.render('index')
+    let newUser = new User({ userId, userInfo }
+        //     {
+        //     id: userId,
+        //     user_first_name: request.body.user_first_name,
+        //     user_last_name: request.body.user_last_name,
+        //     user_email: request.body.user_email,
+        //     user_password: request.body.user_password,
+        //     user_age: request.body.user_age
+        //         // created: Date.now()
+        // }
+    );
+    newUser.save(() => {
+        response.render('signin')
+    });
 })
 app.get('/user', (req, res) => {
     fetch('https://jsonplaceholder.typicode.com/users')
